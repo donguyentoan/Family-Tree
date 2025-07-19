2f90db4c50f20dee902234809e65f79842086f8c
donguyentoan2003 <donguyentoan2003@gmail.com>
2025-07-19 13:10:17 +0700

    update

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link, Outlet } from 'react-router-dom'
 import Header from './Header'
 
+
 const Layout = () => {
   return (
     <div className='bg-main header-giapha' >
@@ -10,7 +11,7 @@ const Layout = () => {
         <Header/>
       </nav>
 
-      <Outlet />
+      <Outlet/>
     </div>
   )
 }
